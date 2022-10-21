4e07d13b6b8848c0d56efe9a7437861e03f1a394
pengshouhua <shouhua@outlook.com>
2022-10-21 11:29:05 +0800

    feat: add debug hello.s config files

--- sample/compile/hello.s
+++ sample/compile/hello.s
@@ -1,28 +1,27 @@
+	.file	"hello.c"
 	.text
-	.globl _message
+	.globl	message
 	.data
-	.align 2
-_message:
-	.ascii "hello, world!\12\0"
-	.globl _buf
-	.zerofill __DATA,__common,_buf,1024,5
+	.align 4
+	.type	message, @object
+	.size	message, 15
+message:
+	.string	"hello, world!\n"
+	.globl	buf
+	.bss
+	.align 32
+	.type	buf, @object
+	.size	buf, 1024
+buf:
+	.zero	1024
 	.text
-	.globl _main
-_main:
-	subl	$12, %esp
-	call	___x86.get_pc_thunk.ax
-L1$pb:
-	subl	$12, %esp
-	leal	_message-L1$pb(%eax), %eax
-	pushl	%eax
-	call	_printf
-	addl	$16, %esp
+	.globl	main
+	.type	main, @function
+main:
+	pushl	$message
+	call	printf
+	addl	$4, %esp
 	movl	$0, %eax
-	addl	$12, %esp
 	ret
-	.weak_definition	___x86.get_pc_thunk.ax
-	.private_extern	___x86.get_pc_thunk.ax
-___x86.get_pc_thunk.ax:
-	movl	(%esp), %eax
-	ret
-	.subsections_via_symbols
+	.size	main, .-main
+	.section	.note.GNU-stack,"",@progbits
