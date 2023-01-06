050357b993bd22cd16d04b6b0385302d6b2baa44
pengshouhua <shouhua@outlook.com>
2023-01-06 19:55:27 +0800

    feat[WIP]: add elf loader doc

---
+++ sample/elf_loader/dump_stack.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <elf.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+    int i = 0;
+    Elf64_auxv_t *auxv = NULL;
+    long *auxv_start_addr = NULL;
+    long *auxv_end_addr = NULL;
+    long *env_start_addr = NULL;
+    long *env_end_addr = NULL;
+    long *argv_end_addr = NULL;
+    char *platform;
+    long *p = (long *)argv;
+    char *env_str = NULL;
+    unsigned char *rand_bytes = NULL;
+    int arg_num = *(p - 1);
+    printf("arg_num:%d\n", arg_num);
+    for (i = 0;i < arg_num;i++) {
+        printf("arg %d:%s\n", i, *p); 
+        p++;
+    }
+    argv_end_addr = p;
+    p++;//skip null
+    printf("\nenvironment vector:\n");
+    env_str = (char *)*p;
+    env_start_addr = p;
+    while (*p) {
+        printf("%s\n", *p); 
+        p++;
+    }
+    env_end_addr = p;
+    p++;//skip null
+    printf("\nauxiliary vectors:\n");
+    auxv = (Elf64_auxv_t *)p; 
+    auxv_start_addr = (long *)auxv;
+    while (auxv->a_type != AT_NULL) {
+        printf("auxv type:%ld, val:0x%x\n", auxv->a_type, auxv->a_un.a_val); 
+        if (auxv->a_type == AT_PLATFORM) {
+            platform = (char *)(auxv->a_un.a_val);
+        }
+        auxv++;
+    }
+    auxv_end_addr = (long *)auxv;
+    auxv++;//skip AT_NULL vector
+    printf("\naddress:%lx, env0_ascii_string:%s\n", env_str, env_str);
+    printf("address:%lx ~ %lx, argument0_ascii_string:%s\n", *argv, argv[0] + strlen(argv[0]),*argv);
+    printf("address:%lx ~ %lx, here contain %ld bytes data\n", platform + strlen(platform) + 1, (char *)*argv - 1, (long)*argv - (long)platform - strlen(platform) - 1);
+    printf("address:%lx ~ %lx, String identifying platform:%s\n", platform, platform + strlen(platform), platform);
+    rand_bytes = platform - 16; 
+    printf("address:%lx ~ %lx, rand_bytes[16]:", rand_bytes, rand_bytes + 15);
+    for (i = 0;i < 16;i++) {
+        printf(" %d", *(rand_bytes + i)); 
+    }
+    printf("\naddress:%lx ~ %lx, auxivilary vector\n", auxv_start_addr, auxv_end_addr);
+    printf("address:%lx ~ %lx, environment vector\n", env_start_addr, env_end_addr);
+    printf("address:%lx ~ %lx, argv, argv pointer: %lx\n", argv, argv_end_addr, *argv);
+    printf("address:%lx, argc\n", argv - 1);
+    return 0;
+}
---
+++ sample/elf_loader/loader_stack.md
@@ -0,0 +1,65 @@
+execve
+.interp (fill the GOT and PLT table)
+_start
+
+stack content:
+...(high)
+local variables of main
+saved registers of main
+return address of main
+argc
+argv
+envp
+stack from startup code
+argc
+argv pointers
+NULL that ends argv[]
+environment pointers
+NULL that ends envp[]
+ELF Auxiliary Table
+argv strings
+environment strings
+program name
+NULL(low)
+
+https://www.mgaillard.fr/2021/04/15/load-elf-user-mode.html
+open file
+read file
+check elf file validation
+apply memory by using mmap (sys/mmap), because need to set memory attributes like read/write/private etc
+memset memory
+
+loop program headers
+check type PT_LOAD
+check filesz <= memsz
+check filesz > 0
+copy PT_LOAD content to mmap area
+set memory attribute
+
+Relocate global dynamic symbols
+loop section with SHT_REL type
+loop relocation section items
+loop relation item 
+r_offset, r_info(r_dynsym_index, r_type)
+find global symbol string: relocation item -> dynamic symbol[r_dynsym_index].st_name -> .dynstr+st_name
+resolve string: 
+handle = dlopen("libc.so.6", RTLD_NOW);
+real_addr = dlsym(handle, sym);
+	
+*relocation_item.r_offset = real_addr
+实际例子中，比如puts，relocation里面的r_offset指向.got.plt中puts的对应项
+
+find the main function in the symbol table and execute
+查找.symtab(因为是本地main所以使用使用symtab，而不是dynsym)
+.symtab -> sh_link 对应的.symstr section index
+.symstr = src + shdr[.symstr section index].sh_offset
+loop .symtab items
+.symtab_item.st_name+.symstr == "main"
+main_addr = .symtab_item.st_value + mmap_start_addr
+main_addr(argc, argv, envp)
+
+https://github.com/malisal/loaders/blob/master/elf/elf.c
+
+get random bytes(16 bytes)
+allocate stack space 8M(mmap)
+load elf file to stack memory
